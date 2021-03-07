0c1385f37b41def9f57f6d1a957525d770ee1afc
Rafhael Payão <51467263+rafaeltmbr@users.noreply.github.com>
2021-03-07 10:47:11 -0300

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Blockly Get Started
+This is a simple example of how to start using blockly in web application. A live version of this repository can be found [here](https://rafaeltmbr.github.io/blockly-get-started). To learn more about it, checkout Blockly [documentation](https://developers.google.com/blockly/guides/get-started/web).
+
+## About Blockly
+[Blockly](https://developers.google.com/blockly) is a client-side library for the programming language JavaScript for creating block-based visual programming languages and editors. A project of Google, it is free and open-source software released under the Apache License 2.0. It typically runs in a web browser, and visually resembles the language Scratch. [Wikipedia](https://en.wikipedia.org/wiki/Blockly)
+
+## Live demo
+
+A live demo can be found at https://rafaeltmbr.github.io/blockly-get-started
+
+![Blockly Get Started live demo image](./assets/img/blockly-hello-world-example.png "Live demo")
---
+++ index.js
@@ -0,0 +1,42 @@
+window.addEventListener("load", () => {
+  const codeContainer = document.querySelector(".code-container");
+
+  // create Blockly workspace
+  const workspace = Blockly.inject("blocklyDiv", {
+    comments: true,
+    disable: false,
+    collapse: false,
+    media: "./media/",
+    readOnly: false,
+    rtl: false,
+    scrollbars: true,
+    toolbox: document.getElementById("toolbox"),
+    toolboxPosition: "start",
+    horizontalLayout: false,
+    sounds: true,
+    zoom: {
+      controls: true,
+      wheel: true,
+      startScale: 0.675,
+      maxScale: 4,
+      minScale: 0.25,
+      scaleSpeed: 1.1,
+    },
+    colours: {
+      fieldShadow: "rgba(255, 255, 255, 0.3)",
+      dragShadowOpacity: 0.6,
+    },
+  });
+
+  function showXmlStructure() {
+    const xml = Blockly.Xml.workspaceToDom(workspace);
+    codeContainer.innerText = Blockly.Xml.domToPrettyText(xml);
+  }
+
+  // insert the start code into Blockly workspace area (optional)
+  Blockly.Xml.domToWorkspace(document.getElementById("startBlocks"), workspace);
+
+  // keep track of every workspace change and update the XML representation
+  workspace.addChangeListener(showXmlStructure);
+  showXmlStructure();
+});
